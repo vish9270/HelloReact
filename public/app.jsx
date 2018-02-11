843975f5365701c5a3de9a58996b93ceef56508b
vish9270 <vishveshu143@gmail.com>
2018-02-11 22:53:43 +0530

    Initial Commit

---
+++ public/app.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Greeter = require('./components/Greeter');
+
+var firstName = 'Vishvesh';
+
+ReactDOM.render(
+  <Greeter name={firstName}/>,
+  document.getElementById('app')
+);
